1de9887e6be07b2db48968367465f35a7e589ca9
shroida <shroida012@gmail.com>
2023-01-11 12:00:18 +0200

    recreate css code for section home

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active')
 }
+var swiper = new Swiper(".container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true
+  });
